2408d8412ed1f0ada129be7af3d9a5e2a63ce7bf
Ziaul Hasan Hamim <ziaulhasanhamim@outlook.com>
2021-02-05 09:31:45 +0600

    Add Login And Logout Options

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from typing import Dict
 from django.contrib.auth.models import User
+from django.contrib.auth import login as login_user, logout as logout_user
 from django.http import HttpRequest, HttpResponse
 import string
+
 
 def signup(request: HttpRequest) -> HttpResponse:
     context: Dict[str, str] = dict()
@@ -34,6 +36,30 @@ def signup(request: HttpRequest) -> HttpResponse:
             context["uname_error"] = "Username Already Exists"
         else:
             User.objects.create_user(username=uname, email=email, password=password)
-            return redirect("/")
+            return redirect("core:index")
         
     return render(request, "accounts/signup.html", context)
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    context: Dict[str, str] = dict()
+
+    if request.method == "POST":
+        email: str = request.POST.get("email")
+        password: str = request.POST.get("pass")
+        user = User.objects.filter(email__iexact=email)
+
+        if not user.exists():
+            context["email_error"] = "Email Doesn't Exist"
+        elif not user.first().check_password(password):
+            context["pass_error"] = "Password Doesn't Match"
+        else:
+            login_user(request, user.first())
+            return redirect("core:index")
+
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request: HttpRequest) -> HttpResponse:
+    logout_user(request)
+    return redirect("accounts:login")
