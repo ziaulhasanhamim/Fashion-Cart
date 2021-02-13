71db04e09b0f4edaee646c0ff9b2bc94ee89e7c1
Ziaul Hasan Hamim <ziaulhasanhamim@outlook.com>
2021-02-13 11:26:03 +0600

    Add Category Display Options

--- core/views.py
+++ core/views.py
@@ -8,10 +8,10 @@ from .models import Product, Category, Slider, Order, OrderItem
 
 def index(request: HttpRequest) -> HttpResponse :
     context = {
-        "latest_men_products": Product.objects.filter(for_gender="men").order_by("-timestamp")[:20],
-        "latest_women_products": Product.objects.filter(for_gender="women").order_by("-timestamp")[:20],
-        "best_sold_men_products": Product.objects.filter(for_gender="men").order_by("sold")[:20],
-        "best_sold_women_products": Product.objects.filter(for_gender="women").order_by("sold")[:20],
+        "latest_men_products": Product.objects.filter(for_gender="men").order_by("-timestamp").prefetch_related("categories")[:20],
+        "latest_women_products": Product.objects.filter(for_gender="women").order_by("-timestamp").prefetch_related("categories")[:20],
+        "best_sold_men_products": Product.objects.filter(for_gender="men").order_by("sold").prefetch_related("categories")[:20],
+        "best_sold_women_products": Product.objects.filter(for_gender="women").order_by("sold").prefetch_related("categories")[:20],
         "men_categories": Category.objects.filter(featured_in="men").exclude(image='').order_by("?")[:4],
         "women_categories": Category.objects.filter(featured_in="women").exclude(image='').order_by("?")[:4],
         "sliders": Slider.objects.filter(enabled=True),
